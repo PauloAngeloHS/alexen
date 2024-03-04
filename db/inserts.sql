547d2e4bf8b37d14508a916d94bc39088a1ef9ad
Paulo Angelo <paulo.angelo01@etec.sp.gov.br>
2024-03-04 09:10:21 -0300

    Added inserts

---
+++ db/inserts.sql
@@ -0,0 +1,5 @@
+INSERT INTO cliente (nome, cpf, email, telefone, celular, data_nascimento, status , data_cadastro)  VALUES ('Pudim', '11122233344','pudim@pudim.com','991114444','991114444',"2010-01-12",'Sim',"2023-09-12");
+
+INSERT INTO fornecedor (nome, cnpj, email, site, telefone, celular, contato, data_abertura, status, data_cadastro) VALUES ('Purin','11122233344','purin@pudim.com','Purin','991112222','991112222','Purin','2020-10-09','Ativo','2023-12-03');
+
+INSERT INTO usuario (nome, email, senha, celular, acesso, status, data_cadastro) VALUES ('Pudding','pudding@pudim.com','password','991112222','Banana','inativo','2020-01-09');
