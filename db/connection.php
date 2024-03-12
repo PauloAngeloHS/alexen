56f98c04d4d0a6435c9e8dce3e2b001c9a10d0af
Dineinho Miller <somethingdine@gmail.com>
2024-03-12 20:40:16 -0300

    Added the files to fill

--- db/connection.php
+++ db/connection.php
@@ -1,36 +1,42 @@
 <?php
-class Connection {
-	private $hostname = "localhost";
-	private $database = "sw1tb";
-	private $user = "root";
-	private $password = "";
+class Connection
+{
 
 	private static $pdo;
 
-	private function __construc() {}
+	private function __construc()
+	{
+	}
 
-	private static function verifyExtention() {
+	private static function verifyExtention()
+	{
 		$extention = 'pdo_mysql';
 	}
 
-	public static function getInstance() {
+	public static function getInstance()
+	{
+		$hostname = "localhost";
+		$database = "sw1tb";
+		$user = "root";
+		$password = "";
+
 		self::verifyExtention();
 		if (!isset(self::$pdo)) {
 			try {
 				$options = array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAME UTF*');
-				self::$pdo = new \PDO("mysql:host". $this->hostname . "; dbname=" . $this->database . ";", $this->user, $this->password, $options);
+				self::$pdo = new \PDO("mysql:host" . $hostname . "; dbname=" . $database . ";", $user, $password, $options);
 				self::$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-	
-			} catch(PDOException $e) {
+			} catch (PDOException $e) {
 				print "Error:" . $e->getMessage();
 			}
 		}
 		return self::$pdo;
 	}
-	public static function isConnected() {
-		if (self::$pdo):
+	public static function isConnected()
+	{
+		if (self::$pdo) :
 			return true;
-		else:
+		else :
 			return false;
 		endif;
 	}
