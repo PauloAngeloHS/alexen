47fbb10b6c94cc0a98c9567b8583be288513a11e
Dineinho Miller <somethingdine@gmail.com>
2024-03-19 20:14:34 -0300

    Just changed the tab space

--- index.php
+++ index.php
@@ -1,27 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Alexen</title>
-		<link href="css/style.css" rel="stylesheet">
-  		<link rel="icon" type="image/x-icon" href="./img/icon.png"/>
-		
-		<?php 
-		require_once './db/connection.php';
-		include './db/components.php';
-		?>
-	</head>
-	<body>
-		<?php navigation();?>
-		<main>
-			<section id="hero">
 
-			</section>
-			<section id="products">
-				
-			</section>
-		</main>	
-		<?php footer();?>	
-	</body>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Alexen</title>
+	<link href="css/style.css" rel="stylesheet">
+	<link rel="icon" type="image/x-icon" href="./img/icon.png" />
+
+	<?php
+	require_once './db/connection.php';
+	include './db/components.php';
+	?>
+</head>
+
+<body>
+	<?php navigation(); ?>
+	<main>
+		<section id="hero">
+
+		</section>
+		<section id="products">
+
+		</section>
+	</main>
+	<?php footer(); ?>
+</body>
+
 </html>
